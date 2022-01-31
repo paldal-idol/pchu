866c34bed04c531a37ae858c5d16a79066463b4a
coreight98 <maxcha98@naver.com>
2022-01-31 20:08:21 +0900

    feat: add button component and main page layer 버튼 컴포넌트 생성 class :small | big 분류 메인 페이지 레이아웃 생성

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,51 @@
 <script lang="js">
-	export let name;
+	let image = 'img/main.png';
+	import Button from './components/Button.svelte';
+	const options = [{title:'github'},{title:'cat'},{title:'woman'},{title:'man'},{title:'pig'},{title:'flower'}];
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<div class="Container">
+		<h1>pchu</h1>
+		<div class="Options">
+			{#each options as option}
+				<Button title={option.title} className={'Small'}/>
+			{/each}
+		</div>
+		<img src={image} alt={'main image'}/>
+		<Button title={'Generate'} className={'Big'}/>
+		<Button title={'Download'} className={'Big'}/>
+	</div>
 </main>
 
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
 	}
-
+	.Container{
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+	}
+	img{
+		margin: 1em;
+		width: 30%;
+		align-self: center;
+	}
 	h1 {
-		color: #ff3e00;
+		color: #ffff;
 		text-transform: uppercase;
 		font-size: 4em;
-		font-weight: 100;
+		font-weight: bold;
 	}
-
+	.Options{
+		display: flex;
+		justify-content: center;
+		column-gap: 0.3em;
+	}
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
---
+++ src/components/Button.svelte
@@ -0,0 +1,25 @@
+<script>
+    export let title;
+    export let className;
+</script>
+
+<button class={className}>{title}</button>
+
+<style>
+    button{
+        background-color: #B9BBEA;
+        border: none;
+        border-radius: 0.2em;
+        padding: 0.5em;
+    }
+    .Small{
+		
+	}
+    .Big{
+        width: 20%;
+        padding: 0.7em 0.5em;
+        align-self: center;
+        font-weight: bolder;
+        font-size: 2em;
+    }
+</style>
